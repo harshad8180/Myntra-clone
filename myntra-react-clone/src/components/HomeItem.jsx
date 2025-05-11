e8e3a8c1f1cad387691a0846162fe7f2673e5e73
harshad8180 <hpborkar@gmail.com>
2025-05-12 01:43:07 +0530

    adding and removing item from cart functionality created

--- myntra-react-clone/src/components/HomeItem.jsx
+++ myntra-react-clone/src/components/HomeItem.jsx
@@ -1,4 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bagActions } from "../store/bagSlice";
+import { GrAddCircle } from "react-icons/gr";
+import { AiFillDelete } from "react-icons/ai";
+
 const HomeItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const bagItems = useSelector((store) => store.bag);
+  const elementFound = bagItems.indexOf(item.id) >= 0;
+
+  const handleAddToBag = () => {
+    dispatch(bagActions.addToBag(item.id));
+  };
+
+  const handleRemove = () => {
+    dispatch(bagActions.removeFromBag(item.id));
+  };
   return (
     <div className="item-container">
       <img className="item-image" src={item.image} alt="item image" />
@@ -12,12 +28,24 @@ const HomeItem = ({ item }) => {
         <span className="original-price">Rs {item.original_price}</span>
         <span className="discount">({item.discount_percentage}% OFF)</span>
       </div>
-      <button
-        className="btn-add-bag"
-        onClick={() => console.log("item was clicked")}
-      >
-        Add to Bag
-      </button>
+
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-danger btn-add-bag"
+          onClick={handleRemove}
+        >
+          <AiFillDelete /> remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-success btn-add-bag"
+          onClick={handleAddToBag}
+        >
+          <GrAddCircle /> Add to Bag
+        </button>
+      )}
     </div>
   );
 };
--- myntra-react-clone/src/store/index.js
+++ myntra-react-clone/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import fetchStatusSlice from "./fetchStatusSlice.js";
 import itemsSlice from "./itemSlice";
+import bagSlice from "./bagSlice.js";
 
 const myntraStore = configureStore({
   reducer: {
     items: itemsSlice.reducer,
     fetchStatus: fetchStatusSlice.reducer,
+    bag: bagSlice.reducer,
   },
 });
 
