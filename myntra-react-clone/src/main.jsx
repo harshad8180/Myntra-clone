062640962cdf5f7c12b56b073945ea9028a2bce9
harshad8180 <hpborkar@gmail.com>
2025-05-11 22:18:58 +0530

    separating components and fixing UI for redux toolkit

--- myntra-react-clone/src/main.jsx
+++ myntra-react-clone/src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./routes/App.jsx";
 import Bag from "./routes/Bag.jsx";
 import Home from "./routes/Home.jsx";
+import { Provider } from "react-redux";
+import myntraStore from "./store/index.js";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +24,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={myntraStore}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
